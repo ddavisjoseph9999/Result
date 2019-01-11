9810fd2b92b5db2a56e3ca807f58ccfc894f07c8
ddavisjoseph9999 <46556372+ddavisjoseph9999@users.noreply.github.com>
2019-01-11 19:34:54 +1100

    Update Program.cs
    Added file folder checks

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -11,14 +11,24 @@ namespace dotnet_code_challenge
     
         static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection()             
+           var serviceProvider = new ServiceCollection()             
              .AddSingleton<IFileHandler, FileHandler>()
              .AddSingleton<IOrderedResult, OrderedResult>()
              .BuildServiceProvider();
-
             ShowResult showResult = new ShowResult(serviceProvider);
-            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FeedData");
-            showResult.Init(folderPath);    
+            string folderPath=null;
+            try
+            {
+                folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FeedData");
+                var fileList = Directory.GetFiles(@folderPath);
+                if (fileList.Length == 0)
+                    Console.WriteLine($"ERROR: Location {@folderPath} does not contain data files");
+                showResult.Init(fileList);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: Location {@folderPath} cannot be accessed: {ex.Message}");
+            }               
             Console.Read();
         }
     }
